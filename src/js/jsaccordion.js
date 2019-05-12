4e153d5d24fea29e69a3a17bf484437ec7504c1c
zohrehtb <zohreh.tt@gmail.com>
2019-05-12 09:33:19 +0430

    first step

--- src/js/jsaccordion.js
+++ src/js/jsaccordion.js
@@ -2,8 +2,43 @@ function JSAccordion(elementOrSelector) {
     if(!(this instanceof JSAccordion))
         return new JSAccordion(elementOrSelector);
 
+
+
     //  define public methods
     this.init = function() {
+
+        var acc_li , acc_ul , acc_header , acc_body , acc_p;
+
+        acc_ul = this.targetElement.querySelector("ul");
+
+        acc_li = this.targetElement.querySelectorAll("li");
+        acc_header = this.targetElement.querySelectorAll("div.header");
+        acc_body = this.targetElement.querySelectorAll("div.body");
+        acc_p = this.targetElement.querySelectorAll("p");
+
+/*
+        console.log(acc_ul);
+*/
+        
+        acc_ul.classList.add("jsacc-list");
+
+
+        acc_li.forEach(function (elemnet) {
+            elemnet.classList.add("jsac-list-item");
+        });
+
+        acc_header.forEach(function (elemnet) {
+            elemnet.classList.add("jsac-header");
+        });
+
+        acc_body.forEach(function (elemnet) {
+            elemnet.classList.add("jsac-body");
+        });
+
+        acc_p.forEach(function (elemnet) {
+            elemnet.classList.add("jsac-p");
+        });
+        
     };
 
     //  start construction operations
